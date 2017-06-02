f096bac634565dc2c9e99421e4466122a5e5e20f
robsonklima <robsonklima@gmail.com>
2017-06-01 23:25:59 -0300

    cordova plugin

--- js/controllers/todoAddCtrl.js
+++ js/controllers/todoAddCtrl.js
@@ -1,15 +1,11 @@
 angular.module("app").controller("todoAddCtrl",
   function ($scope, $http, $location, $mdToast, todoAPIService) {
 
-  $scope.breadcrumbs = {
-      title: 'App Name . Todos . Add',
-      url: 'todo.get'
-  };
-
   $scope.insert = (obj) => {
       todoAPIService.insert(obj).success((data) => {
         $mdToast.show($mdToast.simple().textContent(`Item ${data.text} added.`)
           .hideDelay(1000).position('top right'));
+          $scope.todo = null;
       }).error((data, status, headers, config) => {
         $mdToast.show($mdToast.simple().textContent('Unable to insert item.')
           .hideDelay(1000).position('top right'));
