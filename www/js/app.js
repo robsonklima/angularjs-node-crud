0ef9c9c2de90564fe5c3c0920d296f9552efe03a
robsonklima <robsonklima@gmail.com>
2017-06-22 22:30:22 -0300

    Full Network and GPS Watch

--- www/js/app.js
+++ www/js/app.js
@@ -1,8 +1,9 @@
 var app = angular.module("app", ["ngRoute", "ngMessages", "ngMaterial", "ngCordova", "uiGmapgoogle-maps"])
-  .run(['$rootScope', '$location', '$http', '$cordovaNetwork', function ($rootScope, $location, $http, $cordovaNetwork) {
-
-    // Verify Network Connection
+  .run(['$rootScope', '$location', '$http', '$cordovaNetwork', '$cordovaGeolocation', function ($rootScope, $location, $http, $cordovaNetwork, $cordovaGeolocation) {
+    
     document.addEventListener("deviceready", function () {
+        
+        // Watch Network
         $rootScope.isConnected = $cordovaNetwork.isOnline();
         $rootScope.$on('$cordovaNetwork:online', function(event, networkState){
           $rootScope.isConnected = true;
@@ -10,6 +11,24 @@ var app = angular.module("app", ["ngRoute", "ngMessages", "ngMaterial", "ngCordo
         $rootScope.$on('$cordovaNetwork:offline', function(event, networkState){
           $rootScope.isConnected = false;
         });
+        
+        // Watch Geolocation
+        function getLocation() {
+          var posOptions = {timeout: 20000, enableHighAccuracy: true, repeatOn: 10000};
+          $cordovaGeolocation.getCurrentPosition(posOptions).then(function(position) {
+              $rootScope.myLatitude = position.coords.latitude;
+              $rootScope.myLongitude = position.coords.longitude;
+          }, function(error) {
+              $rootScope.myLatitude = null;
+              $rootScope.myLongitude = null;
+          }).finally(function() {
+              // finally
+          });
+          
+          setTimeout(getLocation, posOptions.repeatOn);
+        }
+
+        getLocation();
     }, false);
 
 }]);
